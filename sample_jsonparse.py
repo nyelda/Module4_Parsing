06d813e0dfebc4470e079ceb117541591c239fd4
NetAcad DEVASC <changeme@example.com>
2023-10-05 13:54:47 +0000

    Added New Code

--- sample_jsonparse.py
+++ sample_jsonparse.py
@@ -1,9 +1,14 @@
 import json
-import xml.etree.cElementTree as et
 
 with open('sample.json', 'r') as json_file:
+    json_data = json.load(json_file)
 
-json_data = json.load(json_file)
-print(json_data)
-print(json.dumps(json_data, indent=4))
-print(Courses)
+Courses = []
+
+certifications = json_data.get('certifications',[])
+
+for certification in certifications:
+    courses = certification.get('courses', [])
+    Courses.extend(courses)
+
+print(','.join(Courses))
